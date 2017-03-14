0f5a1f088d167b863e9d7451c517cdaf24dfaa05
Jacob <jake.sieberg@mail.nidec.com>
2017-03-14 16:00:32 -0500

    added travis.yml

--- cmake/clang-cxx-dev-tools.cmake
+++ cmake/clang-cxx-dev-tools.cmake
@@ -2,8 +2,6 @@ foreach(file ${src})
     Set(fsrc ${fsrc} ${CMAKE_CURRENT_SOURCE_DIR}/${file})
 endforeach(file)
 
-file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/logs)
-
 # Adding clang-format target if executable is found
 find_program(CLANG_FORMAT "clang-format")
 if(CLANG_FORMAT)
@@ -29,7 +27,6 @@ if(CLANG_TIDY)
     --
     -std=c++11
     ${INCLUDE_DIRECTORIES}
-    | tee ${CMAKE_BINARY_DIR}/logs/tidy.txt
     )
 endif()
 
@@ -42,6 +39,5 @@ if(CPPCHECK)
     COMMAND /usr/bin/cppcheck
     ${fsrc}
     ${INCLUDE_DIRECTORIES}
-    | tee ${CMAKE_BINARY_DIR}/logs/check.txt
     )
 endif()
