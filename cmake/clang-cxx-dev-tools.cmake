8d5d719d80479bda9344d8ff844d63795030b216
Jacob <jake.sieberg@mail.nidec.com>
2017-03-21 16:09:22 -0500

    Attempt 25

--- cmake/clang-cxx-dev-tools.cmake
+++ cmake/clang-cxx-dev-tools.cmake
@@ -2,18 +2,18 @@ foreach(file ${src})
     Set(fsrc ${fsrc} ${CMAKE_CURRENT_SOURCE_DIR}/${file})
 endforeach(file)
 
-# # Adding clang-format target if executable is found
-# find_program(CLANG_FORMAT "clang-format")
-# if(CLANG_FORMAT)
-#   add_custom_target(
-#     format
-#     ALL
-#     COMMAND /usr/bin/clang-format
-#     -i
-#     -style=file
-#     ${fsrc}
-#     )
-# endif()
+# Adding clang-format target if executable is found
+find_program(CLANG_FORMAT "clang-format")
+if(CLANG_FORMAT)
+  add_custom_target(
+    format
+    ALL
+    COMMAND /usr/bin/clang-format
+    -i
+    -style=file
+    ${fsrc}
+    )
+endif()
 
 # Adding clang-tidy target if executable is found
 find_program(CLANG_TIDY "clang-tidy")
@@ -30,14 +30,14 @@ if(CLANG_TIDY)
     )
 endif()
 
-# # Adding clang-tidy target if executable is found
-# find_program(CPPCHECK "cppcheck")
-# if(CPPCHECK)
-#   add_custom_target(
-#     check
-#     ALL
-#     COMMAND /usr/bin/cppcheck
-#     ${fsrc}
-#     ${INCLUDE_DIRECTORIES}
-#     )
-# endif()
+# Adding clang-tidy target if executable is found
+find_program(CPPCHECK "cppcheck")
+if(CPPCHECK)
+  add_custom_target(
+    check
+    ALL
+    COMMAND /usr/bin/cppcheck
+    ${fsrc}
+    ${INCLUDE_DIRECTORIES}
+    )
+endif()
