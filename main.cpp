c0b31bc1ee68f84ce0eff7ac325cf124a0ba0ce3
Jacob Sieberg <jake.sieberg@mail.nidec.com>
2017-03-24 14:50:39 -0500

    Readded issues

--- main.cpp
+++ main.cpp
@@ -6,6 +6,10 @@ using namespace std;
 
 int main() {
   cout << "Hello World!" << endl;
+  string s = "Hello Strings\n";
+  printf(s.c_str());
 
-  return 0;
+  char a[10];
+  a[10] = 'f';
+  return a[4];
 }
