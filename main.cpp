be44c46f015175ef7e08747971b1045c1c8dd801
Jacob Sieberg <jake.sieberg@mail.nidec.com>
2017-03-24 15:13:31 -0500

    No Errs

--- main.cpp
+++ main.cpp
@@ -1,15 +1,8 @@
 #include <iostream>
-#include <stdio.h>
-#include <string>
 
 using namespace std;
 
 int main() {
   cout << "Hello World!" << endl;
-  string s = "Hello Strings\n";
-  printf(s.c_str());
-
-  char a[10];
-  a[10] = 'f';
-  return a[4];
+  return 0;
 }
