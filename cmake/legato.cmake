5a0a54b9540a996b07160ff7b4e7d3694e2094c3
Jacob <jake.sieberg@mail.nidec.com>
2017-04-18 10:31:03 -0500

    Removed source command from legato setup

--- cmake/legato.cmake
+++ cmake/legato.cmake
@@ -1,6 +1,4 @@
 #cmake .. -DSETUP=legato
 
-execute_process(COMMAND source /opt/swi/y17-ext/environment-setup-armv7a-vfp-neon-poky-linux-gnueabi)
-
 set(CMAKE_C_COMPILER arm-poky-linux-gnueabi-gcc)
 set(CMAKE_CXX_COMPILER arm-poky-linux-gnueabi-g++)
